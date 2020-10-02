c4ecbda394579e460ca9911f1a03ec468a6a8b17
Evans Obeng <iamevansobeng@outlook.com>
2020-10-02 17:41:19 +0000

    Spinner and data loading @ users

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,27 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-class Useritem extends Component {
-    state = {
-        id: '1',
-        login: "mojombo",
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: "https://github.com/mojombo",
-
-    }
-
-    render() {
-        const { avatar_url, login, html_url } = this.state
-        return (
-            <div className='cart text-center'>
-                <img src={avatar_url} alt="" className='round-img' style={{ width: '60px' }} />
-                <h3>{login} </h3>
-                <div className="">
-                    <a href={html_url} className="bnt btn-dark btn-sm my-1">More</a>
-                </div>
+const UserItem = ({ user: { avatar_url, login, html_url } }) => {
+    return (
+        <div className="card text-center">
+            <img src={avatar_url} alt={login + 'image'} className='round-img' style={{ width: '60px' }} />
+            <h3>{login} </h3>
+            <div>
+                <a href={html_url} className="bnt btn-dark btn-sm my-1">More</a>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
-
-export default Useritem
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
+}
+export default UserItem
