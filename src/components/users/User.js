ef968d9afc29a1cfee59cb5c47796ec980d34e2f
Evans Obeng <iamevansobeng@outlook.com>
2020-10-03 19:31:07 +0000

    Everything working fine in Class Based Components

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -2,21 +2,26 @@ import React, { Component } from 'react'
 import Spinner from '../layout/Spinner'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import Repos from '../repos/Repos'
+
 
 export class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login)
+        this.props.getUserRepos(this.props.match.params.login)
     }
 
     static propTypes = {
         loading: PropTypes.bool.isRequired,
         user: PropTypes.object.isRequired,
         getUser: PropTypes.func.isRequired,
+        getUserRepos: PropTypes.func.isRequired,
+        repos: PropTypes.array.isRequired,
     }
     render() {
         const { name, avatar_url, bio, blog, login, html_url, followers, following, location, public_repos, public_gists, company, hireable } = this.props.user
 
-        const { loading } = this.props
+        const { loading, repos } = this.props
         if (loading) return <Spinner />
         else return (
             <>
@@ -55,6 +60,7 @@ export class User extends Component {
                     <div className="badge badge-danger">Public Repos : {public_repos} </div>
                     <div className="badge badge-dark">Public Gist : {public_gists} </div>
                 </div>
+                <Repos repos={repos} />
             </>
         )
     }
