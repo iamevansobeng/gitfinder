0cb5d04ca2d30b2d81a15bfb91abdbbbe4d8f843
Evans Obeng <iamevansobeng@outlook.com>
2020-10-06 11:53:45 +0000

    Moved to home and search to home component and built notfound for routes that does not exist

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
-import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
 import About from './components/pages/About'
 import User from './components/users/User'
 import './App.css'
 import GithubState from './context/github/GithubState'
 import AlertState from './context/alert/AlertState'
+import Home from './components/pages/Home'
+import NotFound from './components/pages/NotFound'
 
 
 const App = () => {
@@ -22,14 +22,10 @@ const App = () => {
                         <div className="container">
                             <Alert />
                             <Switch>
-                                <Route exact path='/' render={props =>
-                                    <>
-                                        <Search />
-                                        <Users />
-
-                                    </>} />
+                                <Route exact path='/' component={Home} />
                                 <Route exact path='/about' component={About} />
                                 <Route exact path='/user/:login' component={User} />
+                                <Route component={NotFound} />
                             </Switch>
 
                         </div>
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Link } from 'react'
+
+const NotFound = () =>
+    <>
+        <h1 className='lead' style={{ textAlign: 'center' }}>Page not Found</h1>
+        <p style={{ fontSize: '70px', padding: 20, margin: 5, letterSpacing: 12, textAlign: "center", background: 'black', color: 'white' }}>4<i className="far fa-frown" />4</p>
+
+        <a href="/" className='btn btn-primary'> <i className="fas fa-home" /> Go home</a>
+
+    </>
+
+export default NotFound
