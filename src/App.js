415ed4ed7b6393df2b2495d9dbd245a6d6fb3904
Evans Obeng <iamevansobeng@outlook.com>
2020-10-06 09:29:07 +0000

    Moved Repos to Context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import axios from 'axios'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
@@ -12,20 +11,7 @@ import GithubState from './context/github/GithubState'
 
 
 const App = () => {
-    const [repos, setRepos] = useState([])
-    const [loading, setLoading] = useState(false)
     const [alert, setAlert] = useState(null)
-
-
-    const getUserRepos = async (username) => {
-        setLoading(true)
-        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-
-        setRepos(res.data)
-        setLoading(false)
-    }
-
-
 
     const showAlert = (msg, type) => {
         setAlert({ msg, type })
@@ -46,9 +32,7 @@ const App = () => {
 
                                 </>} />
                             <Route exact path='/about' component={About} />
-                            <Route exact path='/user/:login' render={props => (
-                                <User {...props} getUserRepos={getUserRepos} repos={repos} />
-                            )} />
+                            <Route exact path='/user/:login' component={User} />
                         </Switch>
                         <Alert alert={alert} />
 
